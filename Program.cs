051e46677da4e742a7a6e7305f21b1a9a94ef013
Lucas Silva <lucasafs@estudante.ufscar.br>
2026-08-11 08:17:51 -0300

    Desafio POO concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado!!! TESTES
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Nokia Tijolao", "1111111111111111", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("987654", "iPhone 12", "2222222222222222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
